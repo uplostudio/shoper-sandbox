3a9407e44201a932a1f2f477db1a9f138444bb1e
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-20 14:02:14 +0100

    fix

--- trial-step-2.js
+++ trial-step-2.js
@@ -229,27 +229,3 @@ createTrialStepTwo.forEach((n) => {
     }
   });
 });
-
-// $("[app='create_trial_step2']").on("submit", function (event) {
-//   event.preventDefault();
-//   event.stopPropagation();
-//   const errorInfo = $(this).find(".w-form-fail");
-//   $.ajax({
-//     url: "https://www.shoper.pl/ajax.php",
-//     headers: {},
-//     method: "POST",
-//     data: {
-//       action: "create_trial_step2",
-//       phone: $(this).find("[app='phone']").val(),
-//     },
-//     success: function (data) {
-//       console.log(data);
-//       if (data.status === 1) {
-//         window.location.href = "https://www.shoper.pl/zaloz-sklep/";
-//       } else {
-//         errorInfo.css("display", "block");
-//         errorInfo.html("Podany numer jest nieprawidłowy");
-//       }
-//     }
-//   });
-// });
