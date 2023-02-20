b6f417e466005fe7b4ce735ce4bfaf59f40158f2
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-20 13:26:07 +0100

    fix

--- pricelist.js
+++ pricelist.js
@@ -1,20 +1,29 @@
-window.addEventListener("load", ()=>{
-  $("[app='open_create_trial_step1_modal_button']").on("click", function() {
-      $("[app='create_trial_step1_modal']").addClass("modal--open");
-      $(document.body).css("overflow", "hidden");
+window.addEventListener("load", () => {
+  $("[app='open_create_trial_step1_modal_button']").on("click", function () {
+    $("[app='create_trial_step1_modal']").addClass("modal--open");
+    $(document.body).css("overflow", "hidden");
+  });
+  $("[app='open_custom_modal_button']").on("click", function () {
+    $("[app='bannerModal']").addClass("modal--open");
+    $(document.body).css("overflow", "hidden");
   });
   // form + checkbox
   let toggleForm = document.querySelector("[app='toggle_form']");
   let toggle = document.querySelector("[app='toggle_button']");
   let checkbox = toggleForm.querySelector("input[type='checkbox']");
   // promo prices
-  let priceBoxStandard = document.querySelector("[pricebox='standard']").children[0];
-  let priceBoxPremium = document.querySelector("[pricebox='premium']").children[0];
-  let priceBoxEnterprise = document.querySelector("[pricebox='enterprise']").children[0];
+  let priceBoxStandard = document.querySelector("[pricebox='standard']")
+    .children[0];
+  let priceBoxPremium = document.querySelector("[pricebox='premium']")
+    .children[0];
+  let priceBoxEnterprise = document.querySelector("[pricebox='enterprise']")
+    .children[0];
   // regular prices
   let priceBoxStandardRegular = document.querySelector("[regular='standard']");
   let priceBoxPremiumRegular = document.querySelector("[regular='premium']");
-  let priceBoxEnterpriseRegular = document.querySelector("[regular='enterprise']");
+  let priceBoxEnterpriseRegular = document.querySelector(
+    "[regular='enterprise']"
+  );
   // labels
   let boxLabelStandard = document.querySelector("[pricelabel='standard']");
   let boxLabelPremium = document.querySelector("[pricelabel='premium']");
@@ -23,46 +32,44 @@ window.addEventListener("load", ()=>{
   // fetch
 
   $.ajax({
-      url: "https://www.shoper.pl/ajax.php",
-      headers: {},
-      method: "POST",
-      data: {
-          action: "get_prices_list",
-      },
-      success: function(data) {
-          let regular = data.price;
-          let promotion = data.promotion;
+    url: "https://www.shoper.pl/ajax.php",
+    headers: {},
+    method: "POST",
+    data: {
+      action: "get_prices_list",
+    },
+    success: function (data) {
+      let regular = data.price;
+      let promotion = data.promotion;
 
-          priceBoxStandard.textContent = "25"
+      priceBoxStandard.textContent = "25";
+      priceBoxPremium.textContent = regular.premium[1].net;
+      priceBoxEnterprise.textContent = regular.enterprise[1].net;
+
+      console.log(data);
+
+      toggle.addEventListener("click", () => {
+        checkbox.click();
+
+        if (checkbox.checked) {
+          boxLabelStandard.textContent = "brutto miesięcznie";
+          boxLabelPremium.textContent = "brutto miesięcznie";
+          boxLabelEnterprise.textContent = "brutto miesięcznie";
+          priceBoxStandard.textContent = "30.75";
+          priceBoxPremium.textContent = regular.premium[1].gross;
+          priceBoxEnterprise.textContent = regular.enterprise[1].gross;
+        } else {
+          boxLabelStandard.textContent = "netto miesięcznie";
+          boxLabelPremium.textContent = "netto miesięcznie";
+          boxLabelEnterprise.textContent = "netto miesięcznie";
+          priceBoxStandard.textContent = "25";
           priceBoxPremium.textContent = regular.premium[1].net;
           priceBoxEnterprise.textContent = regular.enterprise[1].net;
-
-          console.log(data);
-
-          toggle.addEventListener("click", ()=>{
-              checkbox.click();
-
-              if (checkbox.checked) {
-                  boxLabelStandard.textContent = "brutto miesięcznie";
-                  boxLabelPremium.textContent = "brutto miesięcznie";
-                  boxLabelEnterprise.textContent = "brutto miesięcznie";
-                  priceBoxStandard.textContent = "30.75"
-                  priceBoxPremium.textContent = regular.premium[1].gross;
-                  priceBoxEnterprise.textContent = regular.enterprise[1].gross;
-              } else {
-                  boxLabelStandard.textContent = "netto miesięcznie";
-                  boxLabelPremium.textContent = "netto miesięcznie";
-                  boxLabelEnterprise.textContent = "netto miesięcznie";
-                  priceBoxStandard.textContent = "25"
-                  priceBoxPremium.textContent = regular.premium[1].net;
-                  priceBoxEnterprise.textContent = regular.enterprise[1].net;
-              }
-          }
-          );
-      },
-      error: function(err) {
-          console.log(err);
-      },
+        }
+      });
+    },
+    error: function (err) {
+      console.log(err);
+    },
   });
-}
-);
+});
