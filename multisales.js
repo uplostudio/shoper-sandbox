336535a1d9e4b69f29fbd6754d0c58462ebf0541
SergiuszR <sergiuszrozycki@icloud.com>
2023-04-18 19:51:07 +0200

    added multisales

--- multisales.js
+++ multisales.js
@@ -2,6 +2,7 @@ $("[app='open_multisales_modal_button']").on("click", function () {
   $("[app='multisales_modal']").addClass("modal--open");
 });
 
+console.log(fbclid, gclid);
 //  grab form
 formWrappers = document.querySelectorAll("[app='form_lp']");
 // grab form trigger
@@ -23,7 +24,8 @@ formWrappers.forEach((n) => {
     e.preventDefault();
     e.stopPropagation();
 
-    formWrapper = this.form;
+    formWrapper = e.target.closest("form");
+    loader = formWrapper.querySelector(".loading-in-button");
     phoneInput = formWrapper.querySelector("[app='phone']");
     emailInput = formWrapper.querySelector("[app='email']");
     urlInput = formWrapper.querySelector("[app='url']");
@@ -33,6 +35,7 @@ formWrappers.forEach((n) => {
     checkEmailBlur();
 
     if (outcomeOne && outcomeTwo && outcomeThree) {
+      loader.style.display = "block";
       $.ajax({
         url: "https://www.shoper.pl/ajax.php",
         headers: {},
@@ -45,16 +48,16 @@ formWrappers.forEach((n) => {
           form_name: action,
           thulium_id: 52,
           zapier: "aHR0cHM6Ly9ob29rcy56YXBpZXIuY29tL2hvb2tzL2NhdGNoLzQ5Mjc4OS8zb2gxc29nLw==",
-          fbclid: fbclidValue,
-          gclid: gclidValue,
         },
         success: function (data) {
           if (data.status === 1) {
+            loader.style.display = "none";
             n.querySelector("form").style.display = "none";
             n.parentElement.querySelector(".w-form-done").style.display = "block";
             n.parentElement.querySelector(".w-form-done").textContent = "Sprawdź wiadomość, którą właśnie od nas otrzymałeś!";
             n.querySelector("form").reset();
           } else {
+            loader.style.display = "none";
             n.parentElement.querySelector(".w-form-fail").style.display = "block";
           }
         },
